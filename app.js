60e4f43a19f0eeaa01c51a66e4c21e3ccd204455
LardTom <44534151+LardTom@users.noreply.github.com>
2022-08-16 16:28:39 +0200

    init

---
+++ app.js
@@ -0,0 +1,62 @@
+function redirekt(id){window.location.href = `/rezept.html?id=${id}`;}
+
+
+fetch("./rezepte.json")
+.then(response => {
+   return response.json();
+})
+.then(element => {
+
+    let container=document.querySelector(".container")
+
+    element.forEach(element => {
+        let button = document.createElement('div')
+        button.setAttribute("onclick", `redirekt(${element.id})`)
+        console.log(element.id)
+        button.classList.add("rezept")
+
+        let img = document.createElement("img")
+        let details = document.createElement("div")
+        let heading = document.createElement("span")
+        let yield = document.createElement("span")
+        let cookingTime = document.createElement("span")
+        let kcal = document.createElement("span")
+        let protein = document.createElement("span")
+        let fat = document.createElement("span")
+
+        img.setAttribute("src", element.img)
+        heading.innerHTML = element.name
+        yield.innerHTML = `Für ${element.yield} Personen`
+        cookingTime.innerHTML = `${element.cookingTime}min`
+        kcal.innerHTML = `${element.kcal}g`
+        protein.innerHTML = `${element.protein}g`
+        fat.innerHTML = `${element.fat}g`
+
+        img.classList.add("rezeptImg")
+        heading.classList.add("rezeptHeading")
+        yield.classList.add("rezeptYield")
+        cookingTime.classList.add("rezeptCookingTime")
+        kcal.classList.add("rezeptKcal")
+        protein.classList.add("rezeptProtein")
+        fat.classList.add("rezeptFat")
+        details.classList.add("details")
+
+
+
+        button.appendChild(img)
+        button.appendChild(details)
+
+        details.appendChild(heading)
+        details.appendChild(yield)
+        details.appendChild(cookingTime)
+        details.appendChild(kcal)
+        details.appendChild(protein)
+        details.appendChild(fat)
+
+
+        container.appendChild(button)
+    });
+}
+)
+
+
---
+++ rezept.js
@@ -0,0 +1,67 @@
+
+
+
+fetch("./rezepte.json")
+.then(response => {
+   return response.json();
+})
+.then(element => {
+
+    let urlParams = new URLSearchParams(window.location.search);
+    let rezeptID = urlParams.get('id');
+
+  
+    let name= document.querySelector("#name")
+    let tags= document.querySelector("#tags")
+    let author= document.querySelector("#author")
+    let yield= document.querySelector("#yield")
+    let prepTime= document.querySelector("#prepTime")
+    let kcal= document.querySelector("#kcal")
+    let ingredients= document.querySelector("#ingredients")
+    let steps= document.querySelector("#steps")
+
+    element.forEach(rezept => {
+        
+        if (rezept.id == rezeptID) {
+
+        name.innerHTML = rezept.name
+
+        author.innerHTML = rezept.author
+        yield.innerHTML = rezept.yield
+        prepTime.innerHTML = `${rezept.cookingTime}min`
+        kcal.innerHTML = `${rezept.kcal}kcal`
+
+        let amount = rezept.ingredientsAmount
+        let type = rezept.ingredientsType
+
+        for (let i = 0; i < amount.length; i++) {
+            const element = amount[i];
+            
+            console.log(`${element} of ${type[i]}`);
+
+            let ingredientRow = document.createElement("tr")
+            let ingredient1 = document.createElement("td")
+            let ingredient2 = document.createElement("td")
+
+            ingredient1.innerHTML = element
+            ingredient2.innerHTML = type[i]
+
+            ingredientRow.appendChild(ingredient1)
+            ingredientRow.appendChild(ingredient2)
+
+            ingredients.appendChild(ingredientRow)
+
+        }
+
+        for (let j = 0; j < rezept.step.length; j++) {
+            const StepElement = rezept.step[j];
+            let stepWrapper = document.createElement('li')
+            stepWrapper.innerHTML = StepElement
+            console.log(StepElement);
+            steps.appendChild(stepWrapper)
+        }
+
+        }
+    });
+    
+});
